46734d4f5699caac5f1bc0368c42706960912933
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-01 18:03:06 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict";  // treat all JS code as newer version
+
+// alert( 3 + 3 )  // we are using nodejs, not browser
+
+console.log( 3 + 3 )
+
+console.log("Dewanand")
+
+let name = "dewanand"
+let age = 18
+let isLoggedIn = false
+let State;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value  / empty
+// undefined =>
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "dewanand");
+
+console.log(typeof age);
+
+console.log(typeof null);  // object
+
+console.log(typeof undefined);  // undefined
+
+
+
+
