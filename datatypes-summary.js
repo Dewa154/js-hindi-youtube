7b57e1164cd8266fa9fa57a8a42e1038c347a5ff
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-02 12:00:11 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,23 @@
+// Primitive datatype
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100   // Typescript   const score:number = 100   //  datatype must asign
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3456543576654356754n   // if write n in last then automatically became bigInt
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
