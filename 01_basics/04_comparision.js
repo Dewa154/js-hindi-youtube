edce1b1d09635a0f2859b37e4cb6882f35567e2b
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-02 11:33:10 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);     // Typescript bhi ek language hai.
+// console.log("02" > 1);    // always prefer to use same datatypes
+
+console.log(null > 0);     // false
+console.log(null == 0);    // false
+console.log(null >= 0);    // true
+
+console.log(undefined == 0);    // false
+console.log(undefined > 0);     // false
+console.log(undefined < 0);     // false
+
+// ===  // strict check that means datatype also will be check.
+
+console.log("2" === 2);
+
+
+
