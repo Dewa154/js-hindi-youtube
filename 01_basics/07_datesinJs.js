b095dd891afe32b822ba5f0d2987632b3d258105
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-03 10:54:46 +0000

    07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,15 +1,32 @@
 // Dates
 
 let myDate = new Date()         // new is mandatory
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.toJSON());
-console.log(typeof myDate);    // datatype is Object
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);    // datatype is Object
 
-// let myCreatedDate = new Date(2023, 0, 23)  // Months start from 0.
-let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-console.log(myCreatedDate.toLocaleString());   // This is always prefer to formate.
+// // let myCreatedDate = new Date(2023, 0, 23)  // Months start from 0.
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2024-01-14")
+let myCreatedDate = new Date("01-14-2024")
+// console.log(myCreatedDate.toLocaleString());   // This is always prefer to formate.
 
+let myTimeStamp = Date.now()
 
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+`${newDate.getDate()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
