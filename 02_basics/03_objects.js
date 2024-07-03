a495eb1e64205ccef6694b5fa9671421fee783cd
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-03 15:18:13 +0000

    02_basics/03_objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,27 @@
+// singleton   // single object   //construct se singletonm banta hai
+// Object.create
+
+// object literals
+const JsUser = {
+    name: "Dewanand",   // name = "name" aisa declare hota hai, isme any one koi sa bhi object Write kar sakte hai.
+    "full name": "Dewanand Kurrey",
+    age: 18,
+    location: "Jaipur",
+    email: "Dewanand@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+ } 
+
+ console.log(JsUser.email)
+ console.log(JsUser["email"])
+ console.log(JsUser["full name"]);
+
+ 
+
+
+
+
+
+
+
+
