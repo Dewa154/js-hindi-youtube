0625a9097aaf89597a72196af15f1cd7c79f4a19
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-03 16:52:36 +0000

    02_basics/03_objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,9 +2,14 @@
 // Object.create
 
 // object literals
+
+const mySym = Symbol("key1")
+
+
 const JsUser = {
     name: "Dewanand",   // name = "name" aisa declare hota hai, isme any one koi sa bhi object Write kar sakte hai.
     "full name": "Dewanand Kurrey",
+    [mySym]: "mykey1",   // [] use for write symbol value in object literals
     age: 18,
     location: "Jaipur",
     email: "Dewanand@google.com",
@@ -12,9 +17,27 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
  } 
 
- console.log(JsUser.email)
- console.log(JsUser["email"])
- console.log(JsUser["full name"]);
+//  console.log(JsUser.email)
+//  console.log(JsUser["email"])
+//  console.log(JsUser["full name"]);
+//  console.log(JsUser[mySym]);
+
+ JsUser.email = "Dewanand@chatgpt.com"
+//  Object.freeze(JsUser)
+ JsUser.email = "Dewanand@microsoft.com"
+//  console.log(JsUser);
+
+ JsUser.greeting = function(){
+    console.log("Hello JS User");
+ }
+ JsUser.greetingTwo = function(){
+   console.log(`Hello JS User, ${this.name}`);
+}
+
+ console.log(JsUser.greeting());
+ console.log(JsUser.greetingTwo());
+
+
 
  
 
