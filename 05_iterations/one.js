8197383cee03804dd6851a3ed3fc9f5ea2720f32
Dewanand Kurrey <160487908+Dewa154@users.noreply.github.com>
2024-07-06 05:30:31 +0000

    05_iterations/one.js\

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -17,12 +17,39 @@ for (let i = 0; i < 10; i++) {
 
 // console.log(element);    //  element is not defined
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loop value: ${i}`);
-    for (let j = 0; j <=  10; j++) {
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <=  10; j++) {
     //  console.log(`Inner loop value ${j} and inner loop ${i}`);
-       console.log(i + '*' + j + ' = ' + i*j ); 
+    //  console.log(i + '*' + j + ' = ' + i*j ); 
+
     }
     
 }
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);                    // 3
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+  //  console.log(element);                
+}
+
+
+for (let index = 0; index < myArray.length; index) {
+    const element = myArray[index];
+//    console.log(element);                // unlimited flash print.    // stop through Ctrl + D
+
+}
+
+
+// break and continue
+
+for (let index = 1; index <=20; index++) {
+    console.log(`Value of i is ${index}`);
+    
+}
+
+
+
 
